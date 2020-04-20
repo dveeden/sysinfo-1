d20016611d3366423d9be309b19b87f87edeee3f
Allen Zhong <moeallenz@gmail.com>
2020-04-20 13:11:36 +0800

    cmd: remove root user check in example

--- cmd/sysinfo/main.go
+++ cmd/sysinfo/main.go
@@ -11,21 +11,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os/user"
 
 	"github.com/AstroProfundis/sysinfo"
 )
 
 func main() {
-	current, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if current.Uid != "0" {
-		log.Fatal("requires superuser privilege")
-	}
-
 	var si sysinfo.SysInfo
 
 	si.GetSysInfo()
